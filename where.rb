0af4d9691db8ce6a561d85203ed68407df6c0c0d
Edward Earnest <edwardearnest2015@gmail.com>
2013-09-05 13:13:56 -0500

    brief spec of the initial ui

---
+++ where.rb
@@ -0,0 +1,9 @@
+# by default, initial startup:
+# 1. how much did you make this month?
+# 2. how much did you spend today?
+# eventually can be configured for daily/weekly income and prompt after +1, +7 days/every monday etc.
+# 
+# by default, subsequent startup for remainder of this month
+# 1. how much did you spend today?
+#
+# However, this inital model doesn't account for interest income or investment income etc. TODO
